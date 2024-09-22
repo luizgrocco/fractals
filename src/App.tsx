95d67c80edb22298d074cfe482670ddd19321986
Luiz Rocco <luizgustavorocco@gmail.com>
2024-09-22 16:44:05 -0300

    Adds loading spinner while environment loads

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,20 @@ import {
 import { JesuralemCube } from "./components/3d/jerusalem";
 import { Controls } from "./components/custom-ui/controls";
 import { Suspense } from "react";
+import { LoadingSpinner } from "./components/ui/spinner";
 // import { Debug } from "./components/3d/debug";
 
 function App() {
   return (
     <>
-      <Suspense fallback={"loading environment..."}>
+      <Suspense
+        fallback={
+          <div className="bg-slate-400 w-full h-full flex justify-center items-center flex-col">
+            <h1 className="text-3xl pb-3">Loading Assets ... </h1>
+            <LoadingSpinner size={200} />
+          </div>
+        }
+      >
         <Controls />
         <Canvas className="bg-slate-400">
           <PerspectiveCamera
