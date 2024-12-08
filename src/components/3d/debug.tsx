096d6929f7a183f6cfbb360e2fd7abf5d6a6801d
Luiz Rocco <luizgustavorocco@gmail.com>
2024-12-08 14:21:45 -0300

    dump

--- src/components/3d/debug.tsx
+++ src/components/3d/debug.tsx
@@ -1,61 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Mesh, Object3D, Vector3 } from "three";
 import { Button } from "../ui/button";
 import { Html } from "@react-three/drei";
 import { useControlsStore } from "@/store/controls";
-import { Axiom, LSystem, Rules } from "@/models/lsystem";
 import { useThree } from "@react-three/fiber";
-
-const MOVE_HALVES = "½▽◻△◻②";
-const SQUARE_SIDES = 4;
-
-const SMALLER_CUBES_CENTER_LAYER = "[◻→◻▽◼]→".repeat(SQUARE_SIDES);
-const SMALLER_CUBES_UPPER_LOWER_LAYER = "[◻▽◼]→".repeat(SQUARE_SIDES);
-const LARGER_CUBES_UPPER_LOWER_LAYER =
-  `${MOVE_HALVES}→${MOVE_HALVES}→` + "◼▽◻△→".repeat(SQUARE_SIDES);
-
-interface LSystemData {
-  axiom: Axiom;
-  rules: Rules;
-  angleInDegrees: number;
-  length: number;
-}
-
-const LSYSTEM_DATA: LSystemData = {
-  axiom: "◼",
-  rules: {
-    // Alphabet:
-    //
-    // ←: left
-    // →: right
-    // ↑: up
-    // ↓: down
-    // ⟲ or ⟳: reverse direction (turn 180°)
-    // ◼: draw a box and move
-    // ◻: just move (without drawing a box)
-    // ▽: decrease scale by the given cube scale factor
-    // △: increase scale by the given cube scale factor
-    // ½: decrease scale by a factor of 2
-    // ②: increase scale by a factor of 2
-    // [: save current transformation state (in stack)
-    // ]: restore previous transformation state (from stack)
-    //
-    "◼": `
-      [${SMALLER_CUBES_CENTER_LAYER}]
-      [↑◻↓ ${SMALLER_CUBES_UPPER_LOWER_LAYER}]
-      [↑${MOVE_HALVES}↓ ${LARGER_CUBES_UPPER_LOWER_LAYER}]
-      [↓◻↑ ${SMALLER_CUBES_UPPER_LOWER_LAYER}]
-      [↓${MOVE_HALVES}↑ ${LARGER_CUBES_UPPER_LOWER_LAYER}]
-      ◻◻▽◻△
-    `,
-    "◻": `◻◻▽◻△`,
-  },
-  angleInDegrees: 90,
-  length: 1,
-};
-
-const lsystem = new LSystem(LSYSTEM_DATA.axiom, LSYSTEM_DATA.rules);
 
 const direction = new Vector3(0, 0, 0);
 
@@ -72,19 +21,7 @@ export const Debug: React.FC = () => {
   const cube = useRef<Mesh>(null);
   const [scaling, setScaling] = useState(1);
 
-  const [generations] = useControlsStore((state) => state.generations);
   const [smallCubeScale] = useControlsStore((state) => state.smallCubeScale);
-
-  const sentence = useMemo(() => {
-    lsystem.reset();
-    let acc = "";
-    for (let i = 0; i < generations; i++) {
-      acc = lsystem.generate();
-    }
-    console.log(acc);
-
-    return acc;
-  }, [generations]);
 
   useEffect(() => {
     cube.current!.lookAt(1, 0, 0);
@@ -93,6 +30,9 @@ export const Debug: React.FC = () => {
   return (
     <>
       <mesh ref={cube as any}>
+        {/* <arrowHelper
+          args={[undefined, undefined, undefined, undefined, undefined, 0.2]}
+        /> */}
         <boxGeometry />
         <meshBasicMaterial color={0x00ff00} />
       </mesh>
